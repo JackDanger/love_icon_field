22ce0d59e9485b298f488bd7dccdbf1a0b294d13
Jack Danger Canty <gitcommit@6brand.com>
2009-03-04 20:53:42 -0800

    extracting scene.lua

--- main.lua
+++ main.lua
@@ -1,33 +1,13 @@
 
 love.filesystem.require 'mouse_velocity.lua'
+love.filesystem.require 'scene.lua'
 pieces = {}
 
 function load()
-  -- video modes - use the second-to-last one
-  local modes = love.graphics.getModes()
-  videoMode = modes[6]
-  print(videoMode.width, videoMode.height)
-  love.graphics.setMode(videoMode.width, videoMode.height, false, false, 0)
   -- Load images.
 	loveImg = love.graphics.newImage("love_ball.png", love.image_pad_and_optimize)
-  -- Create the world.
-  world = love.physics.newWorld(videoMode.width, videoMode.height)
-  world:setGravity(0, 100)
-
-  -- Create ground body.
-  ground = love.physics.newBody(world, 0, 0, 0)
-  ground_shape = love.physics.newRectangleShape(ground, videoMode.width/2, videoMode.height-20, videoMode.width -10, 10)
-  -- add walls that go along the edge but also lean out really high to catch stray particles
-  wall_left    = love.physics.newPolygonShape(ground,
-                                                0, 0,
-                                                0, videoMode.height,
-                                                -50, videoMode.height,
-                                                -50, -500)
-  wall_right   = love.physics.newPolygonShape(ground,
-                                                videoMode.width, 0,
-                                                videoMode.width, videoMode.height,
-                                                videoMode.width + 50, videoMode.height,
-                                                videoMode.width + 50, videoMode.height + 500)
+  -- load the world
+	scene:load()
 end
 
 function update(dt)
@@ -41,7 +21,7 @@ function update(dt)
 end
 
 function draw()
-  love.graphics.polygon(love.draw_line, ground_shape:getPoints())
+  scene:draw()
   for i,piece in ipairs(pieces) do
       love.graphics.draw(loveImg, piece.body:getX(), piece.body:getY(), piece.body:getAngle())
   end
@@ -58,7 +38,6 @@ function mousepressed(x, y, button)
     mouseVelocity:reset()
   end
 end
-
 
 function keypressed(key)
   if key == love.key_escape then
---
+++ scene.lua
@@ -0,0 +1,32 @@
+scene = {}
+
+function scene.draw()
+  love.graphics.polygon(love.draw_line, ground_shape:getPoints())
+end
+
+function scene.load()
+  -- video modes
+  local modes = love.graphics.getModes()
+  -- use (arbitrarily #6)
+  videoMode = modes[6]
+  print(videoMode.width, videoMode.height)
+  love.graphics.setMode(videoMode.width, videoMode.height, false, false, 0)
+  -- Create the world.
+  world = love.physics.newWorld(videoMode.width, videoMode.height)
+  world:setGravity(0, 100)
+
+  -- Create ground body.
+  ground = love.physics.newBody(world, 0, 0, 0)
+  ground_shape = love.physics.newRectangleShape(ground, videoMode.width/2, videoMode.height-20, videoMode.width -10, 10)
+  -- add walls that go along the edge but also lean out really high to catch stray particles
+  wall_left    = love.physics.newPolygonShape(ground,
+                                                0, 0,
+                                                0, videoMode.height,
+                                                -50, videoMode.height,
+                                                -50, -500)
+  wall_right   = love.physics.newPolygonShape(ground,
+                                                videoMode.width, 0,
+                                                videoMode.width, videoMode.height,
+                                                videoMode.width + 50, videoMode.height,
+                                                videoMode.width + 50, videoMode.height + 500)
+end
