164c3e692c68f73627a0f068c1cfd120802e7b34
Jack Danger Canty <gitcommit@6brand.com>
2009-03-04 22:24:40 -0800

    using timer.delta for a smarter, simpler MV
    Mouse velocity seems to have some maximum value allowed.  This
    is a reasonably smooth implementation of 'flinging'

--- mouseVelocity.lua
+++ mouseVelocity.lua
@@ -2,34 +2,24 @@ mouseVelocity = {}
 
 -- clear all but the current mouse coordinates
 function mouseVelocity.reset()
-  recentMouseCoords = {
-    {x = love.mouse.getX(), y = love.mouse.getY()}
-  }
+  recentMouseCoords = {x = love.mouse.getX(), y = love.mouse.getY()}
 end
 
-function mouseVelocity.update()
+function mouseVelocity:update(dt)
   if love.mouse.isDown(love.mouse_left) then
     pieces.collection[#pieces.collection].body:setPosition(love.mouse.getX(), love.mouse.getY())
-    vx, vy = mouseVelocity:calculate()
+    vx, vy = mouseVelocity:calculate(dt)
     pieces.collection[#pieces.collection].body:setVelocity(vx, vy)
   end
 end
 
-function mouseVelocity.calculate()
+function mouseVelocity:calculate(dt)
   currentMouseCoords = {x = love.mouse.getX(), y = love.mouse.getY()}
-  -- if recentMouseCoords is blank or if an argument was passed to this function
-  -- the reset the saved coordinates
-  if not recentMouseCoords then recentMouseCoords = {currentMouseCoords} end
+  if not recentMouseCoords then recentMouseCoords = currentMouseCoords end
   
-  local vx = currentMouseCoords.x - recentMouseCoords[#recentMouseCoords].x
-  local vy = currentMouseCoords.y - recentMouseCoords[#recentMouseCoords].y
+  local vx = currentMouseCoords.x - recentMouseCoords.x
+  local vy = currentMouseCoords.y - recentMouseCoords.y
 
-  -- only save a certain number
-  if #recentMouseCoords >= 50 then
-    table.remove(recentMouseCoords, #recentMouseCoords)
-  end
-  -- add the currentMouseCoords to the front of the table.
-  -- They'll be used in 50 frames
-  table.insert(recentMouseCoords, 1, currentMouseCoords)
-  return vx*5, vy*5
+  return (vx * math.abs(vx)) * dt * 100,
+         (vy * math.abs(vy)) * dt * 100
 end
