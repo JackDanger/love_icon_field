55e48c2602643d8f071d5d32b749ce954b1123f0
Jack Danger Canty <gitcommit@6brand.com>
2009-03-04 21:48:42 -0800

    adding background image scaled to videoMode

--- scene.lua
+++ scene.lua
@@ -2,18 +2,32 @@ scene = {}
 
 function scene.draw()
   love.graphics.polygon(love.draw_line, ground_shape:getPoints())
+  print(videoMode.height,
+        scene.backgroundImage:getHeight(),
+        videoMode.width / scene.backgroundImage:getWidth() * 2,
+        videoMode.height / scene.backgroundImage:getHeight() * 2
+        )
+  love.graphics.draw(scene.backgroundImage,
+                     videoMode.width /2,
+                     videoMode.height /2,
+                     0,
+                     videoMode.width / scene.backgroundImage:getWidth(),
+                     videoMode.height / scene.backgroundImage:getHeight()
+                     )
 end
 
 function scene.load()
   -- video modes
   local modes = love.graphics.getModes()
   -- use (arbitrarily #6)
-  videoMode = modes[6]
-  print(videoMode.width, videoMode.height)
+  videoMode = modes[2]
   love.graphics.setMode(videoMode.width, videoMode.height, false, false, 0)
   -- Create the world.
   scene.world = love.physics.newWorld(videoMode.width, videoMode.height)
   scene.world:setGravity(0, 100)
+
+  -- Add a background image
+	scene.backgroundImage = love.graphics.newImage("background.png")
 
   -- Create ground body.
   ground = love.physics.newBody(scene.world, 0, 0, 0)
