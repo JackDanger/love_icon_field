5d62fee4b6eff5b925f6faa04cbf53d9209a4301
Jack Danger Canty <gitcommit@6brand.com>
2009-03-04 20:34:35 -0800

    set walls and ground based on video mode

--- main.lua
+++ main.lua
@@ -2,17 +2,22 @@
 pieces = {}
 
 function load()
+  -- video modes - use the second-to-last one
+  local modes = love.graphics.getModes()
+  videoMode = modes[6]
+  print(videoMode.width, videoMode.height)
+  love.graphics.setMode(videoMode.width, videoMode.height, false, false, 0)
   -- Load images.
 	loveImg = love.graphics.newImage("love_ball.png", love.image_pad_and_optimize)
   -- Create the world.
-  world = love.physics.newWorld(2000, 2000)
-  world:setGravity(0, 50)
+  world = love.physics.newWorld(videoMode.width, videoMode.height)
+  world:setGravity(0, 100)
 
   -- Create ground body.
   ground = love.physics.newBody(world, 0, 0, 0)
-  wall_left   = love.physics.newPolygonShape(ground, -50, -50, -50, 650, 0, 650, 0, -50)
-  wall_right  = love.physics.newPolygonShape(ground, 800, -50, 800, 650, 850, 650, 850, -50)
-  wall_bottom = love.physics.newPolygonShape(ground, 0, 650, 800, 650, 800, 600, 0, 600)
+  ground_shape = love.physics.newRectangleShape(ground, videoMode.width/2, videoMode.height-20, videoMode.width -10, 10)
+  wall_left    = love.physics.newPolygonShape(ground, 0, 0, 0, videoMode.height, -50, videoMode.height, -50, -50)
+  wall_right   = love.physics.newPolygonShape(ground, videoMode.width, 0, videoMode.width, videoMode.height, videoMode.width + 50, videoMode.height, videoMode.width + 50, videoMode.height +50)
 end
 
 function update(dt)
@@ -26,6 +31,7 @@ function update(dt)
 end
 
 function draw()
+  love.graphics.polygon(love.draw_line, ground_shape:getPoints())
   for i,piece in ipairs(pieces) do
       love.graphics.draw(loveImg, piece.body:getX(), piece.body:getY(), piece.body:getAngle())
   end
